4c5640f55044484ea1fe83e0efe5a4d7e1d6d587
Nikita Glazkov <zitrnik@gmail.com>
2020-04-07 12:14:13 +0300

    DEV: add simple server config

---
+++ server/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const path = require("path");
+const dbConfig = require("./db.config.js");
+
+const app = express();
+
+// Парсинг json
+app.use(bodyParser.json());
+
+// парсит запросы по типу: application/x-www-form-urlencoded
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Обработка статических файлов
+app.use("/", serveStatic(path.join(__dirname, "../dist")));
+
+// создаем соединение с нашей базой данных
+const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
+});
+try {
+  connection.connect((err) => {
+    if (err) {
+      console.warn(err);
+    } else console.log("Успешно соединено с базой данных");
+  });
+} catch (err) {
+  console.warn(err);
+}
+
+// При получении любого пути возвращать index.html из папки dist
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
+// При корневом пути возвращать index.html из папки dist
+app.get("/", (req, res) => {
+  res.sendFile(__dirname, "../dist/index.html");
+});
+
+app.listen(3001, () => {
+  console.log("Сервер запущен на http://localhost:3001");
+});
