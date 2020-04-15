3fe490e9e11164f910ad0295203f978c23bc6b16
Nikita Glazkov <zitrnik@gmail.com>
2020-04-16 01:40:40 +0300

    DEV: add text editor to admin panel

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
 
+let editor;
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  selector: "app-admin",
+  templateUrl: "./admin.component.html",
+  styleUrls: ["./admin.component.css"],
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {}
+  onSaveBtn() {
+    editor
+      .save()
+      .then((outputData) => {
+        console.log("Article data: ", outputData);
+      })
+      .catch((error) => {
+        console.log("Saving failed: ", error);
+      });
   }
+  async ngOnInit() {
+    editor = new EditorJS({
+      /**
+       * Id элемента, который будет содержать редактор
+       */
+      holderId: "editorjs",
+      tools: {
+        header: Header,
+        list: List,
+      },
+      autofocus: true,
+      placeholder: "Напиши сюда лучшую статью!",
+    });
 
+    try {
+      await editor.isReady;
+      console.log("Editor.js корректно загружен");
+    } catch (reason) {
+      console.log(`Editor.js загрузка сломалась по причине ${reason}`);
+    }
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthorizationComponent } from './authorization/authorization.component';
-import { RegistrationComponent } from './registration/registration.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AuthorizationComponent } from "./authorization/authorization.component";
+import { RegistrationComponent } from "./registration/registration.component";
+import { AdminComponent } from "./admin/admin.component";
 
 const routes: Routes = [
-  {path: 'authorization', component: AuthorizationComponent},
-  {path: 'registration', component: RegistrationComponent}
+  { path: "authorization", component: AuthorizationComponent },
+  { path: "registration", component: RegistrationComponent },
+  { path: "admin", component: AdminComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
