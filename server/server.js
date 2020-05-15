f6a226079fe876738bec1f9242dba54e606d2407
nvkolezneva <nvkolezneva@gmail.com>
2020-05-15 09:48:35 +0300

    DEV:recaptcha and comments add

--- server/server.js
+++ server/server.js
@@ -479,6 +479,40 @@ app.delete("/api/comments", function (req, res) {
     }
   );
 });
+app.post('/api/token_validate', (req, res)=>{
+
+  let token = req.body.recaptcha;
+  const secretkey = "6Lcd5PMUAAAAADuWGBBEwGhouabjY-SSWRLB2kUv"; //the secret key from your google admin console;
+
+  //token validation url is URL: https://www.google.com/recaptcha/api/siteverify
+  // METHOD used is: POST
+
+  const url =  `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}&remoteip=${req.connection.remoteAddress}`
+
+  //note that remoteip is the users ip address and it is optional
+  // in node req.connection.remoteAddress gives the users ip address
+
+  if(token === null || token === undefined){
+    res.status(201).send({success: false, message: "Token is empty or invalid"})
+    return console.log("token empty");
+  }
+
+  request(url, function(err, response, body){
+    //the body is the data that contains success message
+    body = JSON.parse(body);
+
+    //check if the validation failed
+    if(body.success !== undefined && !data.success){
+      res.send({success: false, 'message': "recaptcha failed"});
+      return console.log("failed")
+    }
+
+    //if passed response success message to client
+    res.send({"success": true, 'message': "recaptcha passed"});
+
+  })
+
+});
 
 app.listen(3001, () => {
   console.log("Сервер запущен на http://localhost:3001");
