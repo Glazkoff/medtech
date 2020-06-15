27a04af6cb783182c9456fc046f3df1945477437
DaryaBel <d.belyaeva1@gmail.com>
2020-06-16 01:11:29 +0400

    DEV: front part of favourites is ready

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -9,9 +9,12 @@ import { environment } from "../../environments/environment";
 })
 export class FavouritesComponent implements OnInit {
   posts = [];
+  notfound = false;
   constructor(private api: BaseApiService) {}
   env = environment;
   postsarr: any;
+
+  // *** ГОТОВО
   async ngOnInit() {
     let postsarr = await this.getPosts();
     if (Array.isArray(postsarr)) {
@@ -30,47 +33,47 @@ export class FavouritesComponent implements OnInit {
         };
         this.posts.push(el);
       });
-      // for (let i = 0; i <= postsarr.length; i++) {
-      //   console.log(postsarr.length);
-      //   try {
-      //     if (postsarr[i] !== undefined) {
-      //       this.jsonParse(JSON.parse(postsarr[i].content));
-      //     }
-      //   } catch (error) {
-      //     console.log(error);
-      //   }
-      // }
+     
+    } else {
+      this.notfound = true;
     }
   }
+// ****
 
+// *** ГОТОВО
   async getPosts() {
-    // this.NewsServ.getPosts().then(() =>
-    //   (this.NewsServ.posts).forEach(post => this.posts.push(post))
-    // );
+    console.log("Зашли в функцию получения избранного");
     let response;
     try {
-      response = await this.api.get("/posts");
-      console.log("RESPONSE");
+      console.log("Отправили запрос на получение избранных статей");
+      response = await this.api.get(`/favourite-materials`);
+      console.log("Получение избранных статей: ");
       console.log(response);
     } catch (error) {
       console.log(error);
     }
     return response;
   }
-  async deleteFavourite() {
+// ****
+
+// *** ГОТОВО
+  async deleteFavourite(id_materials) {
+    console.log("Зашли в функцию удаления статьи из избранного");
+    try {
+      console.log("Отправили запрос на удаление статьи из избранного");
+      let result = await this.api.delete(`/delete-favourites/${id_materials}`);
+      let index = this.posts.findIndex((el)=>{
+        return el.id==id_materials
+      })
+      console.log("Удаление статьи из избранного в масиве");
+      this.posts.splice(index, 1);
+      if (this.posts.length==0) {
+        this.notfound=true;
+      } 
+    } catch (error) {
+      console.log(error);   
+    }
   }
-  // async jsonParse(cont) {
-  //   let html = "";
-  //   // for (let i = 0; i <= this.posts.length; i++) {
-  //   cont.forEach((content) => {
-  //     switch (content.type) {
-  //       case "paragraph":
-  //         html += `<p>${content.data.text}</p>`;
-  //         break;
-  //     }
-  //   });
-  //   // console.log('html: ', html);
-  //   // document.getElementById('paragraph_content' + this.posts[i].id).innerHTML = html;
-  //   // };
-  // }
+ // ****
+   
 }
